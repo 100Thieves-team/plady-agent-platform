aac4be1a146380fb5a979a0ef415bdd8a75d865d
Junseo Bae <ferrater1013@gmail.com>
2026-06-12 20:18:39 +0900

    chore: 프로젝트 의존성 베이스라인 추가

---
+++ src/test/kotlin/io/plady/_00thieveswikimcp/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.plady._00thieveswikimcp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
