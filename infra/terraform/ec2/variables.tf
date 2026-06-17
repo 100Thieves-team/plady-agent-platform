b9841b99861c8121f96ea7d183d28ee805803004
Junseo Bae <ferrater1013@gmail.com>
2026-06-17 11:46:58 +0900

    Use wiki subdomain in deployment docs

--- infra/terraform/ec2/variables.tf
+++ infra/terraform/ec2/variables.tf
@@ -85,7 +85,7 @@ variable "mcp_port" {
 variable "domain_name" {
   description = "Public DNS name served by Caddy with automatic HTTPS."
   type        = string
-  default     = "plady.kro.kr"
+  default     = "wiki.plady.kro.kr"
 
   validation {
     condition     = length(trimspace(var.domain_name)) > 0
